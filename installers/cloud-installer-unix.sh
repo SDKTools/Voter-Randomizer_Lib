bc51571ffcdafe0e73435cebdf71774e6d403f50
SDKTools <jonaslan495@gmail.com>
2024-05-31 17:24:08 -0300

    Installers fixed

--- installers/cloud-installer-unix.sh
+++ installers/cloud-installer-unix.sh
@@ -2,3 +2,5 @@
 
 curl -fsSL https://fnm.vercel.app/install | bash
 fnm use --install-if-missing 20
+git clone https://github.com/SDKTools/Voter-Randomizer_Lib.git /tmp/libTemp
+npm install /tmp/libTemp
--- installers/cloud-installer-windows.bat
+++ installers/cloud-installer-windows.bat
@@ -2,3 +2,6 @@
 
 winget install Schniz.fnm
 fnm use --install-if-missing 20
+winget install --id Git.Git -e --source winget
+git clone https://github.com/SDKTools/Voter-Randomizer_Lib.git
+npm install .\Voter-Randomizer_Lib
